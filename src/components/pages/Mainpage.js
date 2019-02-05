04641ccf894f8527a67d9ca910b4ddc37010f6b3
Valerie <vrosenhauer@gmail.com>
2019-02-05 12:42:19 -0500

    adding component pages

---
+++ src/components/pages/Mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Mainpage extends React.Component {
+    render() {
+        return (
+            <p>main</p>
+        );
+        }
+    }
+
+export default Mainpage
+    
