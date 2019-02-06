2da9845a68faac8577df997d8fdb37a8b410ac2d
Valerie <vrosenhauer@gmail.com>
2019-02-06 16:16:08 -0500

    fixed book cover bug

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import * as BooksAPI from '../BooksAPI'
+//import { Link } from 'react-router-dom'
+//import * as BooksAPI from '../BooksAPI'
 
 
 class Book extends React.Component {
@@ -15,23 +15,12 @@ class Book extends React.Component {
         console.log(this);
     }
 
-    updateBook(book, shelf) {
-        BooksAPI.update(book, shelf)
-        .then(resp => {
-            this.setState(state => {
-                let copy = state.book;
-                copy.shelf = shelf;
-                this.setState({ book:copy});
-            })
-        });
-      }
-
     render() {
         return (
                 <li>
                    <div className="book">
                      <div className="book-top">
-                       <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
+                     <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || ""}")` }}></div>
                        <div className="book-shelf-changer">
                          <select value={this.state.book.shelf} onChange={(event) => this.addToShelf(this.props.book, event.target.value)}>
                            <option value="move" disabled>Move to...</option>
