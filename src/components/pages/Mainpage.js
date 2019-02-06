29b2c90e50a7d66e9b2b5d71d09cbb7456f76043
Valerie <vrosenhauer@gmail.com>
2019-02-05 22:33:37 -0500

    added shelf states

--- src/components/pages/Mainpage.js
+++ src/components/pages/Mainpage.js
@@ -29,9 +29,9 @@ class Mainpage extends React.Component {
             <div className="list-books-content">
               <div>
                 
-                <Shelf name="Currently Reading" books={this.state.books.filter(b => b.shelf, "currentlyreading")} />
-                <Shelf name="Wants To Read" books={this.state.books.filter(b => b.shelf, "wantstoread")} />
-                <Shelf name="Read" books={this.state.books.filter(b => b.shelf, "read")} />
+                <Shelf name="Currently Reading" books={this.state.books.filter(b => b.shelf === "currentlyreading")} />
+                <Shelf name="Wants To Read" books={this.state.books.filter(b => b.shelf === "wantstoread")} />
+                <Shelf name="Read" books={this.state.books.filter(b => b.shelf === "read")} />
             
                 </div>
                 </div>
